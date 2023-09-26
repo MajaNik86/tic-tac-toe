3970477de4ccfa5bcf268713f25d0ed13aac63d1
MajaNik86 <majanik86@gmail.com>
2023-09-26 12:38:44 +0200

    added logic for start new game button with condition of entering both player names , will add modal for it later

--- scripts/app.js
+++ scripts/app.js
@@ -16,11 +16,13 @@ const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const divErrorElement = document.getElementById('error-input');
 const errorOutputElement = document.getElementById('config-errors');
+const gameAreaElement = document.getElementById('active-game');
 
 
 const editPlayer1Element = document.getElementById('edit-player1-btn');
 const editPlayer2Element = document.getElementById('edit-player2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
+const startNewGameBtnElement = document.getElementById('start-game-btn');
 
 
 editPlayer1Element.addEventListener('click', openPlayerConfig);
@@ -30,3 +32,5 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
