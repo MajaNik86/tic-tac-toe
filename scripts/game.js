98632e8d4375e459d44d7c308a1476c855a028f6
MajaNik86 <majanik86@gmail.com>
2023-09-26 13:18:07 +0200

    tracking what field has been clicked by which player, made gameData const to track values

--- scripts/game.js
+++ scripts/game.js
@@ -17,12 +17,22 @@ function switchPlayer() {
 }
 
 function selectGameField(event) {
-    //which game field has been clicked
+    //what game field has been clicked
     //add symbol of the active player
-    event.target.textContent = players[activePlayer].symbol; //players[0];
-    event.target.classList.add('disabled');
+    const selectedField = event.target
+    const selectedColumn = +selectedField.dataset.col - 1;
+    const selectedRow = +selectedField.dataset.row - 1;
+
+    //dont wanna select a field that already has a symbol inside
+    if (gameData[selectedRow][selectedColumn] > 0) {
+        return;
+    }
+    selectedField.textContent = players[activePlayer].symbol; //players[0];
+    selectedField.classList.add('disabled');
+
+    gameData[selectedRow][selectedColumn] = activePlayer + 1;
+    console.log(gameData)
     //switch to the other player
     switchPlayer();
-    //dont wonna select field that already has a symnbol inside
     //keep track of the selected fileds to which player doeas a field belong
 }
