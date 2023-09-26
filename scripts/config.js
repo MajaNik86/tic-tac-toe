a2bc1d0f80e86ff012205bc6c5abea4bb5f6ec05
MajaNik86 <majanik86@gmail.com>
2023-09-26 12:27:39 +0200

    adding storing and displaying  player name inputs and added close on confirm

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,6 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+    const selectedPlayerId = +event.target.dataset.playerid; //convert to string
+    editedPlayer = selectedPlayerId;
     playerConfigOverlay.style.display = 'block';
     backdropElement.style.display = 'block';
 }
@@ -9,6 +11,7 @@ function closePlayerConfig() {
     // formElement.firstElementChild.classList.remove('error');
     divErrorElement.classList.remove('error');
     errorOutputElement.textContent = '';
+    document.getElementById('playername').value = '';
 }
 
 function savePlayerConfig(event) {
@@ -22,6 +25,14 @@ function savePlayerConfig(event) {
         errorOutputElement.textContent = "Please enter a valid name";
         return;
     }
+    const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
+    updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
 
+    if (editedPlayer === 1) {
+        players[0].name = enteredPlayerName
+    } else {
+        players[1].name = enteredPlayerName
+    }
+    closePlayerConfig();
 
 }
